89d899c00175fb24549f3fc0747ce885eacff868
ZNK <ZNK.kuro@gmail.com>
2019-07-14 12:01:56 -0500

    Initial commit

---
+++ src/domino/Fichas.java
@@ -0,0 +1,20 @@
+/**
+  Archivo: Fichas.java
+  Fecha creación:		Jul 14, 2019
+  Última modificación:	mes d, 2019
+  Versión: 0.1
+  Licencia: GPL
+
+  Autores:	Nicolas Jaramillo Mayor        1840558
+
+  Email:	nicolas.jaramillo@correounivalle.edu.co
+
+*/
+package domino;
+
+/**
+ * 
+ */
+public class Fichas {
+
+}
---
+++ src/domino/Main.java
@@ -0,0 +1,28 @@
+/**
+  Archivo: Main.java
+  Fecha creación:		Jul 14, 2019
+  Última modificación:	mes d, 2019
+  Versión: 0.1
+  Licencia: GPL
+
+  Autores:	Nicolas Jaramillo Mayor        1840558
+
+  Email:	nicolas.jaramillo@correounivalle.edu.co
+
+*/
+package domino;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

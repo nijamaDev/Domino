baefc255bceeed0ded1117f8be8fb2be0cea6794
ZNK <ZNK.kuro@gmail.com>
2019-07-14 19:24:37 -0500

    Arraylist

--- src/domino/Domino.java
+++ src/domino/Domino.java
@@ -1,8 +1,8 @@
 /**
   Archivo: Vista.java
-  Fecha creaci�n:		Jul 14, 2019
-  �ltima modificaci�n:	mes d, 2019
-  Versi�n: 0.1
+  Fecha creación:		Jul 14, 2019
+  Última modificación:	mes d, 2019
+  Versión: 0.1
   Licencia: GPL
 
   Autores:	Nicolas Jaramillo Mayor        1840558
@@ -12,11 +12,46 @@
 */
 package domino;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  * 
  */
 public class Domino extends JFrame {
+	private ImageIcon fichaIcon = null;
+	private String imageRoute = "src/images/";
+	private ArrayList<Ficha> pila;
+	
+	public Domino() {
+		try {
+			for (int i=0; i<28; i++) {
+				fichaIcon = new ImageIcon(ImageIO.read(new File(imageRoute + i + ".png")));
+				pila.add(new Ficha(fichaIcon, i));
+			}
+			
+			/*
+			initGUI();
 
+			 default windows configuration
+			this.setUndecorated(true);
+			pack();
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			this.setVisible(true);
+			*/
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, 
+					"No se ha encontrado la imágen en: \"" + imageRoute + "\"");
+		}
+	}
 }
+

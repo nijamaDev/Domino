aa0b533eef03d625a3b0b829cdee90d2c288f549
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2019-07-15 23:25:42 -0500

    panes a mil

--- src/domino/Ficha.java
+++ src/domino/Ficha.java
@@ -1,15 +1,17 @@
 /**
-  Archivo: Fichas.java
-  Fecha creación:		Jul 14, 2019
-  Última modificación:	mes d, 2019
-  Versión: 0.1
+  Archivo: Vista.java
+  Fecha creaciÃ³n:		Jul 14, 2019
+  Ãšltima modificaciÃ³n:	Jul 15, 2019
+  VersiÃ³n: 0.4
   Licencia: GPL
 
-  Autores:	Nicolas Jaramillo Mayor        1840558
+  Autores:	Nicolas Jaramillo Mayor         1840558
+  			Crhistian Alexander Garcia		1832124
 
   Email:	nicolas.jaramillo@correounivalle.edu.co
-
+  			garcia.crhistian@correounivalle.edu.co
 */
+
 package domino;
 
 import javax.swing.ImageIcon;
--- src/domino/Oponente.java
+++ src/domino/Oponente.java
@@ -1,15 +1,17 @@
 /**
-  Archivo: Oponente.java
-  Fecha creaci�n:		Jul 14, 2019
-  �ltima modificaci�n:	mes d, 2019
-  Versi�n: 0.1
+  Archivo: Vista.java
+  Fecha creación:		Jul 14, 2019
+  Última modificación:	Jul 15, 2019
+  Versión: 0.4
   Licencia: GPL
 
-  Autores:	Nicolas Jaramillo Mayor        1840558
+  Autores:	Nicolas Jaramillo Mayor         1840558
+  			Crhistian Alexander Garcia		1832124
 
   Email:	nicolas.jaramillo@correounivalle.edu.co
-
+  			garcia.crhistian@correounivalle.edu.co
 */
+
 package domino;
 
 /**
